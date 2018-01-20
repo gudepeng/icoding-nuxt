847e80ccbcf8c533a5c55466c42fd395fdd482ba
yzworker <15898193673@163.com>
2018-01-20 08:56:44 +0800

    11

--- api.config.js
+++ api.config.js
@@ -1,7 +1,7 @@
 const isProdMode = Object.is(process.env.NODE_ENV, 'production')
 
 module.exports = {
-  cdnUrl: isProdMode ? 'https://cdn.surmon.me' : 'https://cdn.surmon.me',
+  cdnUrl: isProdMode ? 'https://cdn1.surmon.me' : 'https://cdn.surmon.me',
   baseUrl: isProdMode ? 'https://api.surmon.me/' : 'https://api.surmon.me/',
   socketHost: isProdMode ? 'https://surmon.me' : 'https://surmon.me'
 }
