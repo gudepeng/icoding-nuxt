e6eb4152b8288f50f0c86b2deb937cca06af5afb
gudepeng <gudepeng@rongcapital.cn>
2018-01-26 22:38:39 +0800

    解决跨域

--- components/layout/header.vue
+++ components/layout/header.vue
@@ -7,7 +7,7 @@
         </div>
         <div class="navbar-menu">
           <div>
-            <span class="navbar-slogan">首页</span>
+            <span @click="$router.push('/')" class="navbar-slogan">首页</span>
             <span class="navbar-slogan">开源项目</span>
             <span class="navbar-slogan">分享资源</span>
             <span class="navbar-slogan">视频教程</span>
@@ -22,9 +22,9 @@
             <span class="navbar-slogan" @click="login(1)">注册</span>
           </div>
           <div v-show="userInfo!=null">
-            <span class="navbar-slogan">写文章</span>
+            <span @click="$router.push('/publish')" class="navbar-slogan">写文章</span>
             <span class="navbar-slogan">|</span>
-            <img src="https://gold-cdn.xitu.io/v3/static/img/default-avatar.e30559a.svg"/>
+            <img @click="$router.push('/mycenter')" src="https://gold-cdn.xitu.io/v3/static/img/default-avatar.e30559a.svg"/>
           </div>
         </div>
       </div>
--- plugins/axios.js
+++ plugins/axios.js
@@ -7,7 +7,7 @@ axios.defaults.headers.put['Content-Type'] = 'application/json';
 axios.defaults.headers.delete['Content-Type'] = 'application/json';
 
 const service = axios.create({
-  // baseURL: 'http://39.106.214.109:8081'
+  //baseURL: 'http://39.106.214.109:8081',
   baseURL:'http://127.0.0.1:80',
   withCredentials: true,
 })
